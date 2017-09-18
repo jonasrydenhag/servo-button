d22339dcc55adc6d6039c8dcc0db8386ac03cf72
Jonas <jonasrydenhag@users.noreply.github.com>
2017-09-18 12:49:15 +0900

    Replaces console.log with debug

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+var debug = require('debug')('servoButton');
 var gpio = require('rpi-gpio');
 var servoState = require('../servoState');
 
@@ -12,10 +13,10 @@ gpio.on('change', function(channel, value) {
   if (value === true && promise === undefined) {
     promise = servoState.change()
       .then(function (state) {
-        console.log('Server state changed to "' + state + '"');
+        debug('Server state changed to "' + state + '"');
       })
       .catch(function (ex) {
-          console.log('Error: ' + ex);
+          debug(ex);
       })
       .then(function (state) {
           promise = undefined;
@@ -27,7 +28,10 @@ gpio.setup(pin, gpio.DIR_IN, gpio.EDGE_RISING);
 
 process.on('SIGINT', function () {
   gpio.destroy(function() {
-    console.log('All pins unexported');
+    var msg = "All pins unexported";
+    debug(msg);
+    console.log(msg);
+
     process.exit();
   });
 });
